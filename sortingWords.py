efb72d129cf9be2fdc0be2965d7ef5a422af77ae
Vinchenzo98 <vincent.boardman@fdmgroup.com>
2025-01-29 13:26:49 +0000

    changed code to insert index - still wrong

--- sortingWords.py
+++ sortingWords.py
@@ -2,34 +2,16 @@ def order(sentence):
     if sentence == "":
         return ""
     
-    new_list = list()
-    
     split_by_whitespace = sentence.split(" ")
     whitespace_list = list(split_by_whitespace)
     for i in whitespace_list:
         for j in i:
             if j.isdigit():
-                new_list.append(j)
-                new_list.sort()    
-                print(new_list)
-
-            
-    for k in new_list:
-        for h in whitespace_list: 
-            if k in h:   
-                index_value = whitespace_list.index(h)
-                new_list.insert(index_value, h)
-
-        # if k == j:
-        #     new_list.insert(index_value, i)
-        #     remove_value = index_value + 1
-        #     remove_index = new_list[remove_value]
-        #     new_list.remove(remove_index)  
-        #     print(new_list)
-
-            
+                set_position = int(j)
+                whitespace_list.insert(set_position ,i)
+                string_index = whitespace_list.index(i)
+                whitespace_list.remove(whitespace_list[string_index])
+                print(whitespace_list)
+               
+     
 order("4of Fo1r pe6ople g3ood th5e the2")
-
-
-
-#use mapping to do it in refactor
